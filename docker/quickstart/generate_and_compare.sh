08565d2ebf843c9bef7dc3cecf3b0318720dc520
Enrico Minack <github@enrico.minack.dev>
2021-08-17 09:42:39 +0200

    Add Dgraph to docker, as an alternative to Neo4J
    Elasticsearch backend becomes the default, Neo4J and Dgraph an equivalent alternative to elasticsearch.

--- docker/quickstart/generate_and_compare.sh
+++ docker/quickstart/generate_and_compare.sh
@@ -10,30 +10,26 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp.quickstart.yml
-
-python generate_docker_quickstart.py ../docker-compose-without-neo4j.yml ../docker-compose-without-neo4j.override.yml temp-without-neo4j.quickstart.yml
+python generate_docker_quickstart.py ../docker-compose-with-neo4j.yml ../docker-compose-with-neo4j.override.yml temp-with-neo4j.quickstart.yml
+python generate_docker_quickstart.py ../docker-compose-with-dgraph.yml ../docker-compose-with-dgraph.override.yml temp-with-dgraph.quickstart.yml
 
 python generate_docker_quickstart.py ../monitoring/docker-compose.monitoring.yml temp.quickstart.monitoring.yml
 
-if cmp docker-compose.quickstart.yml temp.quickstart.yml; then
-    printf 'docker-compose.quickstart.yml is up to date.'
-else
-    printf 'docker-compose.quickstart.yml is out of date.'
+for flavour in "" "-with-neo4j" "-with-dgraph"
+do
+  if cmp docker-compose$flavour.quickstart.yml temp$flavour.quickstart.yml; then
+    printf "docker-compose$flavour.quickstart.yml is up to date."
+  else
+    printf "docker-compose$flavour.quickstart.yml is out of date."
     exit 1
-fi
-
-if cmp docker-compose-without-neo4j.quickstart.yml temp-without-neo4j.quickstart.yml; then
-    printf 'docker-compose-without-neo4j.quickstart.yml is up to date.'
-    exit 0
-else
-    printf 'docker-compose-without-neo4j.quickstart.yml is out of date.'
-    exit 1
-fi
+  fi
+done
 
 if cmp docker-compose.quickstart.monitoring.yml temp.quickstart.monitoring.yml; then
-    printf 'docker-compose.quickstart.monitoring.yml is up to date.'
-    exit 0
+  printf "docker-compose.quickstart.monitoring.yml is up to date."
 else
-    printf 'docker-compose.quickstart.monitoring.yml is out of date.'
-    exit 1
+  printf "docker-compose.quickstart.monitoring.yml is out of date."
+  exit 1
 fi
+
+exit 0
