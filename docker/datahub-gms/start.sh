a1aff5c63904799362e4b28f11550f0d4e2ad787
Enrico Minack <github@enrico.minack.dev>
2021-08-20 11:37:04 +0200

    Add dgraph to docker

--- docker/datahub-gms/start.sh
+++ docker/datahub-gms/start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-# Add default URI (http) scheme to NEO4J_HOST if missing
+# Add default URI (http) scheme to NEO4J_HOST and DGRAPH_HOST if missing
 if [[ -n "$NEO4J_HOST" && $NEO4J_HOST != *"://"* ]] ; then
     NEO4J_HOST="http://$NEO4J_HOST"
+fi
+if [[ -n "$DGRAPH_HOST" && $DGRAPH_HOST != *"://"* ]] ; then
+    DGRAPH_HOST="http://$DGRAPH_HOST"
 fi
 
 # Add elasticsearch host url
@@ -32,8 +35,16 @@ fi
 WAIT_FOR_GRAPH_SERVICE=""
 
 if [[ $GRAPH_SERVICE_IMPL == neo4j ]]; then
+  if [[ -z "$NEO4J_HOST" ]]; then
+    echo "GRAPH_SERVICE_IMPL set to neo4j but no NEO4J_HOST set"
+    exit 1
+  fi
   WAIT_FOR_GRAPH_SERVICE=" -wait $NEO4J_HOST "
 elif [[ $GRAPH_SERVICE_IMPL == dgraph ]]; then
+  if [[ -z "$DGRAPH_HOST" ]]; then
+    echo "GRAPH_SERVICE_IMPL set to DGRAPH but no DGRAPH_HOST set"
+    exit 1
+  fi
   WAIT_FOR_GRAPH_SERVICE=" -wait $DGRAPH_HOST "
 fi
 
--- docker/quickstart/generate_and_compare.sh
+++ docker/quickstart/generate_and_compare.sh
@@ -9,13 +9,13 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp.quickstart.yml
+python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp-with-elasticsearch.quickstart.yml
 python generate_docker_quickstart.py ../docker-compose-with-neo4j.yml ../docker-compose-with-neo4j.override.yml temp-with-neo4j.quickstart.yml
 python generate_docker_quickstart.py ../docker-compose-with-dgraph.yml ../docker-compose-with-dgraph.override.yml temp-with-dgraph.quickstart.yml
 
 python generate_docker_quickstart.py ../monitoring/docker-compose.monitoring.yml temp.quickstart.monitoring.yml
 
-for flavour in "" "-with-neo4j" "-with-dgraph"
+for flavour in "-with-elasticsearch" "-with-neo4j" "-with-dgraph"
 do
   if cmp docker-compose$flavour.quickstart.yml temp$flavour.quickstart.yml; then
     printf "docker-compose$flavour.quickstart.yml is up to date."
